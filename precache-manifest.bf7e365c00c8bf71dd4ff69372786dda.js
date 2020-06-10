f4e2a772bf3f38e9836ad3b574512aec861f74b9
chortex <romachorrt@gmail.com>
2020-06-10 20:37:16 +0300

    Updates

---
+++ precache-manifest.bf7e365c00c8bf71dd4ff69372786dda.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185ec7cc8a28bdce31096a1e583421de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4b57fa3057e6c7edc0",
+    "url": "/static/css/2.c9754456.chunk.css"
+  },
+  {
+    "revision": "107be3c3520b81e451d1",
+    "url": "/static/css/main.3d41bde9.chunk.css"
+  },
+  {
+    "revision": "ef4b57fa3057e6c7edc0",
+    "url": "/static/js/2.bd83f6cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bd83f6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107be3c3520b81e451d1",
+    "url": "/static/js/main.c0176d01.chunk.js"
+  },
+  {
+    "revision": "615d0ad7309de9c9c000",
+    "url": "/static/js/runtime-main.8771b6bd.js"
+  },
+  {
+    "revision": "7c84ccf4a085f744a7b0d1db2ffa6f83",
+    "url": "/static/media/20200605172559_1-min.7c84ccf4.jpg"
+  },
+  {
+    "revision": "d2e973e91d3268e84d70a739a13f6f85",
+    "url": "/static/media/boot_blurred.d2e973e9.jpg"
+  },
+  {
+    "revision": "2a0e9cfb0bfe1ffddee1ae26fd9d07bd",
+    "url": "/static/media/boot_clean_grid.2a0e9cfb.jpg"
+  },
+  {
+    "revision": "84266f6afc92cc9e11a311000d0cc0b9",
+    "url": "/static/media/boot_greyscale.84266f6a.jpg"
+  },
+  {
+    "revision": "7c1d2dc085cd963484d3054f821ab5f5",
+    "url": "/static/media/boxesModels1.7c1d2dc0.jpg"
+  },
+  {
+    "revision": "45154b9c255009e74b3f85c8e9a334e7",
+    "url": "/static/media/boxesModels2.45154b9c.jpg"
+  },
+  {
+    "revision": "0f675729a588d5a1020ff0e48f727b32",
+    "url": "/static/media/boxesModels3.0f675729.jpg"
+  },
+  {
+    "revision": "b7e66ecb7084ed749cb7abfabe166b09",
+    "url": "/static/media/contrastBlue.b7e66ecb.jpg"
+  },
+  {
+    "revision": "d903e29c07853940b24e1aa44253174f",
+    "url": "/static/media/contrastRed.d903e29c.jpg"
+  },
+  {
+    "revision": "81af08eb2e5f0f37c5ac1e8f7534a09c",
+    "url": "/static/media/cross_blurred.81af08eb.jpg"
+  },
+  {
+    "revision": "80b4ca891f88e7ff3af33d8c6fefe64e",
+    "url": "/static/media/cross_clean_grid.80b4ca89.jpg"
+  },
+  {
+    "revision": "a3515f9d2dc7b8a718d56f42f18bbb7c",
+    "url": "/static/media/cross_greyscale.a3515f9d.jpg"
+  },
+  {
+    "revision": "834d1808ec8294f3761754d3258a0d45",
+    "url": "/static/media/endcamp_blurred.834d1808.jpg"
+  },
+  {
+    "revision": "afb6d8b97b723df971ce381e3f1bc774",
+    "url": "/static/media/kensanGauss.afb6d8b9.jpg"
+  },
+  {
+    "revision": "0118d64240d97d869d95031783c246e0",
+    "url": "/static/media/kensanHandgun.0118d642.jpg"
+  },
+  {
+    "revision": "22f46d6efe1edea05cfccd04e2964783",
+    "url": "/static/media/kensanWorld1.22f46d6e.jpg"
+  },
+  {
+    "revision": "7d77d8f962062f9cad8776ddbbb628f2",
+    "url": "/static/media/kensanWorld2.7d77d8f9.jpg"
+  },
+  {
+    "revision": "f44c50c64bcf00d2470643a0ef3d995d",
+    "url": "/static/media/logo.f44c50c6.png"
+  },
+  {
+    "revision": "aac2d57e592c192224dff3f391f49498",
+    "url": "/static/media/lv2_blurred.aac2d57e.jpg"
+  },
+  {
+    "revision": "e2111c5b0ba7ae154444d2e2cdfb23a3",
+    "url": "/static/media/lv2_clean_grid.e2111c5b.jpg"
+  },
+  {
+    "revision": "d3dcace43e7a8debcf373d9899f9dd72",
+    "url": "/static/media/lv2_greyscale.d3dcace4.jpg"
+  },
+  {
+    "revision": "141e00e9afe366bab0e3d5b80c4f17ba",
+    "url": "/static/media/rammyBlue.141e00e9.jpg"
+  },
+  {
+    "revision": "b3098afb065a51db062886c268bfd753",
+    "url": "/static/media/rammyGauss.b3098afb.jpg"
+  },
+  {
+    "revision": "ffbda9ea825095a70d1ee9db0f30f38f",
+    "url": "/static/media/rammyHandgun.ffbda9ea.jpg"
+  },
+  {
+    "revision": "9190a196b66ac24c11ba2f69b9057d07",
+    "url": "/static/media/rammyRed.9190a196.jpg"
+  },
+  {
+    "revision": "66894ac2e714cfec978b5dabd32d45c4",
+    "url": "/static/media/stalkx_blurred.66894ac2.jpg"
+  },
+  {
+    "revision": "141f6a9ed0f37ab95bceb3b4edf64de7",
+    "url": "/static/media/stalkx_clean_grid.141f6a9e.jpg"
+  },
+  {
+    "revision": "9af2959277bcbe0e541b8223a8839b78",
+    "url": "/static/media/stalkx_greyscale.9af29592.jpg"
+  },
+  {
+    "revision": "9d7a06dbc1b5600b9b941824a020fe2d",
+    "url": "/static/media/stalky_greyscale.9d7a06db.jpg"
+  },
+  {
+    "revision": "23ae509fbd1749701cabb02184dbaccd",
+    "url": "/static/media/stalkyard_clean_grid.23ae509f.jpg"
+  }
+]);
